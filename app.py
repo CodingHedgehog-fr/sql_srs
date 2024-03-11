70ca7347b8eb97286fcd09b5964b859d29a56362
CodingHedgeHog-fr <ymoignard@netcourrier.com>
2024-03-11 16:05:23 +0100

    add duckdb

--- app.py
+++ app.py
@@ -1,60 +1,43 @@
-import io
-
 import duckdb
-import pandas as pd
 import streamlit as st
-
-csv = """
-beverage, price
-orange juice,2.5
-Expresso,2
-Tea,3
-"""
-
-beverages = pd.read_csv(io.StringIO(csv))
-
-csv2 = """
-food_item, food_price
-cookie juice,2.5
-Chocolatine,2
-Muffin,3
-"""
-
-food_items = pd.read_csv(io.StringIO(csv2))
 
 answer = """
 select * from beverages
 cross join food_items
 """
+con = duckdb.connect(database="data/exercises.duckdb", read_only=False)
 
-solution = duckdb.query(answer).df()
+# solution = duckdb.query(answer).df()
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review",
-        ("joins", "Groupby", "Windows functions"),
+        ("cross_joins", "Groupby", "Windows functions"),
         index=None,
         placeholder="select a theme",
     )
 
-    st.write(f"You have selected : {option}")
+    st.write(f"You have selected : {theme}")
+
+    exercice = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}' ").df()
+    st.write(exercice)
 
 st.header("Enter your code")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 
-if query:
-    result = duckdb.query(query).df()
-    st.dataframe(result)
-
-tab2, tab3 = st.tabs(["Tables", "Solution"])
-
-with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("Expected:")
-    st.dataframe(solution)
-
-with tab3:
-    st.write(answer)
+# if query:
+#     result = duckdb.query(query).df()
+#     st.dataframe(result)
+#
+# tab2, tab3 = st.tabs(["Tables", "Solution"])
+#
+# with tab2:
+#     st.write("table: beverages")
+#     st.dataframe(beverages)
+#     st.write("table: food_items")
+#     st.dataframe(food_items)
+#     st.write("Expected:")
+#     st.dataframe(solution)
+#
+# with tab3:
+#     st.write(answer)
